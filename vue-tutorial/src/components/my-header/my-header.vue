6471c1c256224ac7883429917d12606f259f76c3
JTZ <876737761@qq.com>
2025-05-07 06:06:56 +0800

    44
    44

---
+++ vue-tutorial/src/components/my-header/my-header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">
+    <span class="icon"></span>
+    <h1 class="text">Chicken Music</h1>
+    <router-link class="mine" to="/user">
+      <i class="icon-mine"></i>
+    </router-link>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ vue-tutorial/src/components/my-tab/my-tab.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="tab">
+    <router-link class="tab-item" v-for="tab in tabs" :to="tab.path" :key="tab.path">
+      <span class="tab-text">
+        {{ tab.name }}
+      </span>
+    </router-link>
+  </div>
+</template>
+
+<!-- <script setup lang="ts">
+import { ref } from 'vue'
+const tabs = ref([
+  {
+    name: '推荐',
+    path: '/recommand'
+  }
+])
+</script> -->
+<script setup lang="ts">
+const tabs = [
+  {
+    name: 'item',
+    path: '/item',
+  },
+  {
+    name: '歌手',
+    path: '/singer',
+  },
+  {
+    name: '排行',
+    path: '/top-list',
+  },
+  {
+    name: '搜索',
+    path: '/search',
+  },
+]
+</script>
+<style scoped></style>
