a1cef773e174764032facce67bddcb91a0548f27
Zhang Jintao <133624596+zjt0515@users.noreply.github.com>
2025-06-04 23:28:32 +0800

    77
    7

--- tutorial-nestjs/src/user/user.service.ts
+++ tutorial-nestjs/src/user/user.service.ts
@@ -1,26 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
 
   findAll() {
-    return `This action returns all user`;
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+  find(username: string) {}
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  create(user: User) {}
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  update(id: number, user: Partial<User>) {}
+
+  remove(id: number) {}
 }
